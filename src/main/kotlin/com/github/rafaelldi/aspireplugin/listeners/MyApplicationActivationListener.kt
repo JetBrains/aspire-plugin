87267bcc76be240751057af8deaecc435fd895a2
GitHub Action <action@github.com>
2023-11-24 16:37:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/rafaelldi/aspireplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/rafaelldi/aspireplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.rafaelldi.aspireplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
