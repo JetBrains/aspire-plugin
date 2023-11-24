c27c12ef5c9c51609828e6855a6a2a07f2756a92
Rival Abdrakhmanov <rival.abdrakhmanov@gmail.com>
2023-11-24 18:06:11 +0100

    Cleanup

--- src/main/kotlin/com/github/rafaelldi/aspireplugin/AspireBundle.kt
+++ src/main/kotlin/com/github/rafaelldi/aspireplugin/AspireBundle.kt
@@ -5,9 +5,9 @@ import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
 
 @NonNls
-private const val BUNDLE = "messages.MyBundle"
+private const val BUNDLE = "messages.AspireBundle"
 
-object MyBundle : DynamicBundle(BUNDLE) {
+object AspireBundle : DynamicBundle(BUNDLE) {
 
     @JvmStatic
     fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
