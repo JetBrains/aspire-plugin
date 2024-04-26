cca96a1c400ae7501f8a48c79dd6f9e824775470
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2024-04-26 15:25:37 +0200

    Do not add launchSettings info to the runnable project

--- src/dotnet/aspire-plugin/RunnableProject/AspireRunnableProjectProvider.cs
+++ src/dotnet/aspire-plugin/RunnableProject/AspireRunnableProjectProvider.cs
@@ -1,26 +1,19 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using JetBrains.ProjectModel;
-using JetBrains.ProjectModel.DotNetCore;
 using JetBrains.ProjectModel.Properties;
 using JetBrains.ReSharper.Features.Running;
 using JetBrains.Rider.Model;
 using JetBrains.Util;
 using static AspirePlugin.Project.AspireHostProjectPropertyRequest;
-using Key = JetBrains.Rider.Model.Key;
 
 namespace AspirePlugin.RunnableProject;
 
 [SolutionComponent]
 public class AspireRunnableProjectProvider(
     ProjectRunnableOutputDetector projectRunnableOutputDetector,
-    DotNetCoreLaunchSettingsJsonProfileProvider launchSettingsProvider,
     ILogger logger
 ) : IRunnableProjectProvider
 {
-    private const string DefaultDashboardUrl = "http://localhost:18888";
-    private const string AspnetcoreUrlsEnvVar = "ASPNETCORE_URLS";
-
     public JetBrains.Rider.Model.RunnableProject? CreateRunnableProject(
         IProject project,
         string name,
@@ -45,62 +38,16 @@ public class AspireRunnableProjectProvider(
             projectOutputs.Add(projectOutput);
         }
 
-        var applicationUrl = DefaultDashboardUrl;
-        var launchBrowser = false;
-        var environmentVariables = new List<EnvironmentVariable>();
-
-        var profileContent = GetProfileContent(project);
-        if (profileContent is not null)
-        {
-            applicationUrl = profileContent.ApplicationUrl?.FirstOrDefault(it => it.StartsWith("https"))
-                             ?? profileContent.ApplicationUrl?.FirstOrDefault()
-                             ?? DefaultDashboardUrl;
-            launchBrowser = profileContent.LaunchBrowser;
-
-            environmentVariables.AddRange(
-                profileContent
-                    .EnvironmentVariables
-                    .Select(env => new EnvironmentVariable(env.Key, env.Value))
-            );
-
-            if (!profileContent.EnvironmentVariables.ContainsKey(AspnetcoreUrlsEnvVar))
-            {
-                environmentVariables.Add(new EnvironmentVariable(AspnetcoreUrlsEnvVar, applicationUrl));
-            }
-        }
-
-        var customAttributes = new List<CustomAttribute>
-        {
-            new(Key.StartBrowserUrl, applicationUrl),
-            new(Key.LaunchBrowser, $"{launchBrowser}")
-        };
-
         return new JetBrains.Rider.Model.RunnableProject(
             name,
             fullName,
             project.ProjectFileLocation.NormalizeSeparators(FileSystemPathEx.SeparatorStyle.Unix),
             AspireRunnableProjectKinds.AspireHost,
             projectOutputs,
-            environmentVariables,
+            [],
             null,
-            customAttributes
+            []
         );
-    }
-
-    private LaunchSettingsJson.ProfileContent? GetProfileContent(IProject project)
-    {
-        var hasLaunchSettings = launchSettingsProvider.HasLaunchSettingsJson(project);
-        if (!hasLaunchSettings) return null;
-
-        var profiles = launchSettingsProvider.TryGetLaunchJsonSettingsProfiles(project);
-        var projectProfiles = profiles
-                                  ?.Profiles
-                                  ?.Where(it => it.CommandName == LaunchSettingsJson.ProjectCommand)
-                                  ?.ToList()
-                              ?? EmptyList<LaunchSettingsJson.ProfileContent>.InstanceList;
-        var targetProfile = projectProfiles.FirstOrDefault(it => it.ApplicationUrl.Contains("https"))
-                            ?? projectProfiles.FirstOrDefault();
-        return targetProfile;
     }
 
     public IEnumerable<RunnableProjectKind> HiddenRunnableProjectKinds => EmptyList<RunnableProjectKind>.Instance;
