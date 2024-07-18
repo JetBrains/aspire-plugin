6f504e3215cc022f332490ecdc2f6c3fdaab91e7
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2024-07-18 09:14:52 +0200

    Remove org.gradle.toolchains.foojay-resolver-convention

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,11 +12,6 @@ pluginManagement {
     }
 }
 
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
-
-
 rootProject.name = "aspire-plugin"
 
 include(":protocol")
