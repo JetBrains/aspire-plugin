d543e93ff2d1589c695e7144ead239dccfae529f
denis.kuznetsov <denis.kuznetsov@jetbrains.com>
2024-10-24 17:17:40 +0200

    wip

--- src/dotnet/aspire-session-host/CollectionExtensions.cs
+++ src/dotnet/aspire-session-host/CollectionExtensions.cs
@@ -1,12 +1,13 @@
-extern alias SystemCollections;
-using System.Collections.Generic;
-
 namespace JetBrains.Rider.Aspire.SessionHost;
 
 public static class CollectionExtensions
 {
   public static bool TryAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
   {
-      return SystemCollections::System.Collections.Generic.CollectionExtensions.TryAdd(dictionary, key, value);
+    ArgumentNullException.ThrowIfNull(dictionary);
+    if (dictionary.ContainsKey(key))
+      return false;
+    dictionary.Add(key, value);
+    return true;
   }
 }
