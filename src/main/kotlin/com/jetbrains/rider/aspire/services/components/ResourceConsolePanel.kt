5100988f87ebb8aaf54249ce9916a15b45980395
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2024-11-29 22:41:22 +0100

    Add log parser

--- src/main/kotlin/com/jetbrains/rider/aspire/services/components/ResourceConsolePanel.kt
+++ src/main/kotlin/com/jetbrains/rider/aspire/services/components/ResourceConsolePanel.kt
@@ -7,6 +7,6 @@ import com.jetbrains.rider.aspire.services.AspireResource
 class ResourceConsolePanel(resourceService: AspireResource) : BorderLayoutPanel() {
     init {
         border = JBUI.Borders.empty()
-        add(resourceService.logConsole.component)
+        add(resourceService.logConsoleComponent)
     }
 }
