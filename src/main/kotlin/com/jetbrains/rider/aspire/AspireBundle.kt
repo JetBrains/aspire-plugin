3cc6746c36e1db7546b2b8f6e624d12976a4c227
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2024-12-02 14:07:19 +0100

    Refactor AspireBundle

--- src/main/kotlin/com/jetbrains/rider/aspire/AspireBundle.kt
+++ src/main/kotlin/com/jetbrains/rider/aspire/AspireBundle.kt
@@ -1,18 +1,23 @@
 package com.jetbrains.rider.aspire
 
 import com.intellij.DynamicBundle
+import org.jetbrains.annotations.Nls
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
+import java.util.function.Supplier
 
 @NonNls
 private const val BUNDLE = "messages.AspireBundle"
 
-object AspireBundle : DynamicBundle(BUNDLE) {
+object AspireBundle {
+    private val INSTANCE = DynamicBundle(AspireBundle::class.java, BUNDLE)
+
     @JvmStatic
-    fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) = getMessage(key, *params)
+    fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): @Nls String =
+        INSTANCE.getMessage(key, *params)
 
     @Suppress("unused")
     @JvmStatic
-    fun messagePointer(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
-        getLazyMessage(key, *params)
+    fun messagePointer(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): Supplier<@Nls String> =
+        INSTANCE.getLazyMessage(key, *params)
 }
