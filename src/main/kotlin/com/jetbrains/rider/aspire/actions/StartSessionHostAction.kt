2fd89ddc140f732c829a4062082d08950e3c8ff9
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2025-02-20 12:33:23 +0100

    Add actions to start-stop session host

---
+++ src/main/kotlin/com/jetbrains/rider/aspire/actions/StartSessionHostAction.kt
@@ -0,0 +1,20 @@
+@file:Suppress("UnstableApiUsage")
+
+package com.jetbrains.rider.aspire.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.progress.currentThreadCoroutineScope
+import com.jetbrains.rider.aspire.sessionHost.SessionHostManager
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class StartSessionHostAction : AnAction() {
+    override fun actionPerformed(event: AnActionEvent) {
+        val project = event.project ?: return
+        val manager = SessionHostManager.getInstance(project)
+        currentThreadCoroutineScope().launch(Dispatchers.Default) {
+            manager.getOrStartSessionHost()
+        }
+    }
+}
