ef7ca45eaa30797e023931034148fe2bddee8d77
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2025-02-25 16:05:24 +0100

    Refactoring

--- src/main/kotlin/com/jetbrains/rider/aspire/sessionHost/wasmHost/WasmHostProjectSessionProcessLauncher.kt
+++ src/main/kotlin/com/jetbrains/rider/aspire/sessionHost/wasmHost/WasmHostProjectSessionProcessLauncher.kt
@@ -2,25 +2,33 @@ package com.jetbrains.rider.aspire.sessionHost.wasmHost
 
 import com.intellij.execution.process.ProcessListener
 import com.intellij.ide.browsers.StartBrowserSettings
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
 import com.jetbrains.rd.util.lifetime.Lifetime
-import com.jetbrains.rider.aspire.sessionHost.projectLaunchers.BaseProjectSessionProcessLauncher
+import com.jetbrains.rider.aspire.generated.CreateSessionRequest
+import com.jetbrains.rider.aspire.run.AspireHostConfiguration
+import com.jetbrains.rider.aspire.sessionHost.SessionExecutableFactory
+import com.jetbrains.rider.aspire.sessionHost.projectLaunchers.DotNetExecutableWithHotReloadSessionProcessLauncher
 import com.jetbrains.rider.nuget.PackageVersionResolution
 import com.jetbrains.rider.nuget.RiderNuGetInstalledPackageCheckerHost
 import com.jetbrains.rider.runtime.DotNetExecutable
 import com.jetbrains.rider.runtime.dotNetCore.DotNetCoreRuntime
 import java.nio.file.Path
 
-class WasmHostProjectSessionProcessLauncher : BaseProjectSessionProcessLauncher() {
+/**
+ * Provides support for the Blazor WASM projects.
+ */
+class WasmHostProjectSessionProcessLauncher : DotNetExecutableWithHotReloadSessionProcessLauncher() {
     companion object {
         private const val DEV_SERVER_NUGET = "Microsoft.AspNetCore.Components.WebAssembly.DevServer"
         private const val SERVER_NUGET = "Microsoft.AspNetCore.Components.WebAssembly.Server"
+
+        private val LOG = logger<WasmHostProjectSessionProcessLauncher>()
     }
 
-    override val priority = 1
+    override val priority = 3
 
     override val hotReloadExtension = WasmHostHotReloadConfigurationExtension()
-    override val launchBrowserInDebugSession = false
 
     override suspend fun isApplicable(projectPath: String, project: Project): Boolean {
         val nugetChecker = RiderNuGetInstalledPackageCheckerHost.Companion.getInstance(project)
@@ -65,4 +73,20 @@ class WasmHostProjectSessionProcessLauncher : BaseProjectSessionProcessLauncher(
         sessionProcessLifetime,
         aspireHostProjectPath
     )
+
+    override suspend fun getDotNetExecutable(
+        sessionModel: CreateSessionRequest,
+        isDebugSession: Boolean,
+        hostRunConfiguration: AspireHostConfiguration?,
+        project: Project
+    ): Pair<DotNetExecutable, StartBrowserSettings?>? {
+        val factory = SessionExecutableFactory.getInstance(project)
+        val addBrowserAction = !isDebugSession
+        val executable = factory.createExecutable(sessionModel, hostRunConfiguration, addBrowserAction)
+        if (executable == null) {
+            LOG.warn("Unable to create executable for project: ${sessionModel.projectPath}")
+        }
+
+        return executable
+    }
 }
