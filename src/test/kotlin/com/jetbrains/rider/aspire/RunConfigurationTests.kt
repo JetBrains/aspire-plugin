38e31797e9d696a098dd202de9a393e170684ac3
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2025-03-17 09:45:43 +0100

    Set up a project model timeout

--- src/test/kotlin/com/jetbrains/rider/aspire/RunConfigurationTests.kt
+++ src/test/kotlin/com/jetbrains/rider/aspire/RunConfigurationTests.kt
@@ -3,11 +3,14 @@ package com.jetbrains.rider.aspire
 import com.intellij.execution.RunManagerEx
 import com.jetbrains.rider.aspire.run.AspireHostConfiguration
 import com.jetbrains.rider.aspire.run.AspireHostConfigurationType
+import com.jetbrains.rider.test.OpenSolutionParams
 import com.jetbrains.rider.test.annotations.Solution
 import com.jetbrains.rider.test.annotations.TestEnvironment
 import com.jetbrains.rider.test.base.PerClassSolutionTestBase
 import com.jetbrains.rider.test.env.enums.BuildTool
 import com.jetbrains.rider.test.env.enums.SdkVersion
+import com.jetbrains.rider.test.facades.solution.RiderSolutionApiFacade
+import com.jetbrains.rider.test.facades.solution.SolutionApiFacade
 import com.jetbrains.rider.test.framework.executeWithGold
 import com.jetbrains.rider.test.scriptingApi.maskMachineSpecificPaths
 import org.testng.annotations.Test
@@ -15,6 +18,15 @@ import org.testng.annotations.Test
 @TestEnvironment(sdkVersion = SdkVersion.AUTODETECT, buildTool = BuildTool.AUTODETECT)
 @Solution("DefaultAspireSolution")
 class RunConfigurationTests : PerClassSolutionTestBase() {
+
+    override val solutionApiFacade: SolutionApiFacade = object : RiderSolutionApiFacade() {
+        override fun waitForSolution(params: OpenSolutionParams) {
+            // This may sometimes take a long time on CI agents.
+            params.projectModelReadyTimeout = params.projectModelReadyTimeout.multipliedBy(10L)
+
+            return super.waitForSolution(params)
+        }
+    }
 
     @Test
     fun `A run configuration should be generated for an opened solution`() {
