724df7be5b19d4f318cf3a08d642fd384f7b59b2
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2025-05-14 10:39:12 +0200

    Add a test for manifest generation

--- src/test/kotlin/com/jetbrains/rider/aspire/ManifestGenerationTests.kt
+++ src/test/kotlin/com/jetbrains/rider/aspire/ManifestGenerationTests.kt
@@ -1,5 +1,8 @@
 package com.jetbrains.rider.aspire
 
+import com.jetbrains.rdclient.util.idea.waitAndPump
+import com.jetbrains.rider.aspire.manifest.ManifestService
+import com.jetbrains.rider.projectView.solutionDirectoryPath
 import com.jetbrains.rider.test.OpenSolutionParams
 import com.jetbrains.rider.test.annotations.Solution
 import com.jetbrains.rider.test.annotations.TestEnvironment
@@ -10,10 +13,13 @@ import com.jetbrains.rider.test.facades.solution.RiderSolutionApiFacade
 import com.jetbrains.rider.test.facades.solution.SolutionApiFacade
 import com.jetbrains.rider.test.framework.executeWithGold
 import org.testng.annotations.Test
+import java.time.Duration
+import kotlin.io.path.exists
+import kotlin.io.path.readText
 
 @TestEnvironment(sdkVersion = SdkVersion.AUTODETECT, buildTool = BuildTool.AUTODETECT)
 @Solution("DefaultAspireSolution")
-class RunConfigurationTests : PerClassSolutionTestBase() {
+class ManifestGenerationTests : PerClassSolutionTestBase() {
     override val solutionApiFacade: SolutionApiFacade = object : RiderSolutionApiFacade() {
         override fun waitForSolution(params: OpenSolutionParams) {
             // This may sometimes take a long time on CI agents.
@@ -24,9 +30,23 @@ class RunConfigurationTests : PerClassSolutionTestBase() {
     }
 
     @Test
-    fun `A run configuration should be generated for an opened solution`() {
+    fun `A manifest service should generate an Aspire manifest for a host project`() {
+        val service = ManifestService.getInstance(project)
+        val hostPath = project.solutionDirectoryPath
+            .resolve("DefaultAspireSolution.AppHost")
+            .resolve("DefaultAspireSolution.AppHost.csproj")
+        service.generateManifest(hostPath)
+
+        val manifestFilePath = project.solutionDirectoryPath
+            .resolve("DefaultAspireSolution.AppHost")
+            .resolve("aspire-manifest.json")
+
+        waitAndPump(Duration.ofMinutes(1), {
+            manifestFilePath.exists()
+        })
+
         executeWithGold(testGoldFile) { printStream ->
-            dumpAspireHostRunConfigurations(project, printStream)
+            printStream.print(manifestFilePath.readText())
         }
     }
 }
