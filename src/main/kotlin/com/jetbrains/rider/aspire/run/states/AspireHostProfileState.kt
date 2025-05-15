5cda0e19a7030c15d694e9468603b91b2baf38bd
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2025-05-15 14:41:05 +0200

    Refactor Aspire environment variable accessors and improve configuration handling

--- src/main/kotlin/com/jetbrains/rider/aspire/run/states/AspireHostProfileState.kt
+++ src/main/kotlin/com/jetbrains/rider/aspire/run/states/AspireHostProfileState.kt
@@ -1,20 +1,22 @@
 package com.jetbrains.rider.aspire.run.states
 
-import com.jetbrains.rider.aspire.util.ASPIRE_RESOURCE_SERVICE_ENDPOINT_URL
 import com.jetbrains.rider.aspire.util.DCP_INSTANCE_ID_PREFIX
-import com.jetbrains.rider.aspire.util.DOTNET_DASHBOARD_FRONTEND_BROWSERTOKEN
-import com.jetbrains.rider.aspire.util.DOTNET_DASHBOARD_RESOURCESERVICE_APIKEY
-import com.jetbrains.rider.aspire.util.DOTNET_RESOURCE_SERVICE_ENDPOINT_URL
+import com.jetbrains.rider.aspire.util.getAspireDashboardFrontendBrowserToken
+import com.jetbrains.rider.aspire.util.getAspireDashboardResourceServiceApiKey
+import com.jetbrains.rider.aspire.util.getAspireResourceServiceEndpointUrl
 
 interface AspireHostProfileState {
     val environmentVariables: Map<String, String>
 }
 
-fun AspireHostProfileState.getDcpInstancePrefix() = environmentVariables[DCP_INSTANCE_ID_PREFIX]
+fun AspireHostProfileState.getDcpInstancePrefix() =
+    environmentVariables[DCP_INSTANCE_ID_PREFIX]
 
-fun AspireHostProfileState.getDashboardBrowserToken() = environmentVariables[DOTNET_DASHBOARD_FRONTEND_BROWSERTOKEN]
+fun AspireHostProfileState.getDashboardBrowserToken() =
+    environmentVariables.getAspireDashboardFrontendBrowserToken()
 
 fun AspireHostProfileState.getResourceServiceEndpointUrl() =
-    environmentVariables[ASPIRE_RESOURCE_SERVICE_ENDPOINT_URL] ?: environmentVariables[DOTNET_RESOURCE_SERVICE_ENDPOINT_URL]
+    environmentVariables.getAspireResourceServiceEndpointUrl()
 
-fun AspireHostProfileState.getResourceServiceApiKey() = environmentVariables[DOTNET_DASHBOARD_RESOURCESERVICE_APIKEY]
+fun AspireHostProfileState.getResourceServiceApiKey() =
+    environmentVariables.getAspireDashboardResourceServiceApiKey()
