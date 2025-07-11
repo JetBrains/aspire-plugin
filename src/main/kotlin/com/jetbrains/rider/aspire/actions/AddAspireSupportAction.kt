8642d6d9bd0fb9b4af3cde92a7dc3708eb9f7d4a
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2025-07-11 16:48:38 +0200

    Load AppHost and ServiceDefaults project templates

--- src/main/kotlin/com/jetbrains/rider/aspire/actions/AddAspireSupportAction.kt
+++ src/main/kotlin/com/jetbrains/rider/aspire/actions/AddAspireSupportAction.kt
@@ -2,15 +2,20 @@ package com.jetbrains.rider.aspire.actions
 
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.progress.currentThreadCoroutineScope
 import com.intellij.openapi.project.DumbAwareAction
 import com.jetbrains.rider.aspire.projectTemplates.AspireProjectTemplateService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 internal class AddAspireSupportAction : DumbAwareAction() {
     override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
 
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
-        AspireProjectTemplateService.getInstance(project).createHotProjectFromTemplate()
+        currentThreadCoroutineScope().launch(Dispatchers.Default) {
+            AspireProjectTemplateService.getInstance(project).createHotProjectFromTemplate()
+        }
     }
 
     override fun update(e: AnActionEvent) {
