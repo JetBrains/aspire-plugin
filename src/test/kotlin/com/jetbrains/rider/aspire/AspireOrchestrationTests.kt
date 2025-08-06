75b6ec258b9dc4c8522f2b549fedd1d6af6f7a67
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2025-08-06 10:22:07 +0200

    Add more orchestration tests

--- src/test/kotlin/com/jetbrains/rider/aspire/AspireOrchestrationTests.kt
+++ src/test/kotlin/com/jetbrains/rider/aspire/AspireOrchestrationTests.kt
@@ -7,66 +7,108 @@ import com.jetbrains.rider.aspire.orchestration.AspireOrchestrationService
 import com.jetbrains.rider.projectView.workspace.findProjects
 import com.jetbrains.rider.test.annotations.Solution
 import com.jetbrains.rider.test.annotations.TestEnvironment
+import com.jetbrains.rider.test.asserts.shouldBe
 import com.jetbrains.rider.test.asserts.shouldBeTrue
-import com.jetbrains.rider.test.base.ProjectModelBaseTest
+import com.jetbrains.rider.test.base.PerTestSolutionTestBase
 import com.jetbrains.rider.test.env.enums.BuildTool
 import com.jetbrains.rider.test.env.enums.SdkVersion
 import com.jetbrains.rider.test.framework.executeWithGold
 import com.jetbrains.rider.test.scriptingApi.prepareProjectView
 import com.jetbrains.rider.test.scriptingApi.runBlockingWithFlushing
+import kotlinx.coroutines.delay
 import org.testng.annotations.Test
+import java.io.File
+import java.io.PrintStream
+import java.nio.file.Path
 import kotlin.io.path.exists
+import kotlin.io.path.name
 import kotlin.io.path.readText
 import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
 
 @TestEnvironment(sdkVersion = SdkVersion.AUTODETECT, buildTool = BuildTool.AUTODETECT)
-class AspireOrchestrationTests : ProjectModelBaseTest() {
+class AspireOrchestrationTests : PerTestSolutionTestBase() {
     @Test
     @Solution("DefaultAspNetCoreSolution")
     fun `Add aspire orchestration for asp net core project`() {
+        runTest(listOf(activeSolution), "$activeSolution.AppHost", "$activeSolution.ServiceDefaults")
+    }
+
+    @Test
+    @Solution("SolutionWithMultipleAspNetCoreProjects")
+    fun `Add aspire orchestration for multiple asp net core projects`() {
+        val projectNames = listOf("Project1", "Project2", "Project3")
+        runTest(projectNames, "$activeSolution.AppHost", "$activeSolution.ServiceDefaults")
+    }
+
+    @Test
+    @Solution("DefaultAspNetCoreSolutionWithAppHost")
+    fun `Add aspire orchestration for asp net core project with existing AppHost and ServiceDefaults`() {
+        runTest(listOf(activeSolution), "AppHost", "ServiceDefaults")
+    }
+
+    private fun runTest(projectNames: List<String>, appHostName: String, serviceDefaultsName: String) {
         prepareProjectView(project)
 
         val solutionDirectoryPath = activeSolutionDirectory.toPath()
         val solution = activeSolutionDirectory.resolve("$activeSolution.sln")
-        val projectPath = solutionDirectoryPath.resolve(activeSolution).resolve("$activeSolution.csproj")
+        val projectPaths = projectNames
+            .map { solutionDirectoryPath.resolve(it).resolve("$it.csproj") }
 
         val service = AspireOrchestrationService.getInstance(project)
         runBlockingWithFlushing("Adding .NET Aspire Orchestration", 5.minutes) {
-            val projectEntity = project.serviceAsync<WorkspaceModel>()
+            val projectEntities = project.serviceAsync<WorkspaceModel>()
                 .findProjects()
-                .singleOrNull { it.url?.toPath() == projectPath }
-            requireNotNull(projectEntity) { "No project entity found for $projectPath" }
+                .filter { it.url?.toPath()?.let { path -> projectPaths.contains(path) } ?: false }
+            projectEntities.size.shouldBe(projectNames.size)
 
-            service.addAspireOrchestration(listOf(projectEntity))
+            service.addAspireOrchestration(projectEntities)
+
+            delay(10.seconds)
         }
 
-        val hostProjectFileName = "$activeSolution.AppHost"
-        val hostProjectPath = solutionDirectoryPath.resolve(hostProjectFileName).resolve("$hostProjectFileName.csproj")
+        val hostProjectPath = solutionDirectoryPath.resolve(appHostName).resolve("$appHostName.csproj")
         hostProjectPath.exists().shouldBeTrue()
 
-        val sharedProjectFileName = "$activeSolution.ServiceDefaults"
-        val sharedProjectPath = solutionDirectoryPath.resolve(sharedProjectFileName).resolve("$sharedProjectFileName.csproj")
+        val sharedProjectPath =
+            solutionDirectoryPath.resolve(serviceDefaultsName).resolve("$serviceDefaultsName.csproj")
         sharedProjectPath.exists().shouldBeTrue()
 
         executeWithGold(testGoldFile) { printStream ->
-            printStream.println("Solution file:")
-            printStream.println()
-            printStream.println(solution.readText())
-            printStream.println("AppHost:")
-            printStream.println()
-            printStream.println(hostProjectPath.readText())
-            printStream.println("AppHost.Program.cs:")
-            printStream.println()
-            printStream.println(hostProjectPath.parent.resolve("Program.cs").readText())
-            printStream.println("ServiceDefaults:")
-            printStream.println()
-            printStream.println(sharedProjectPath.readText())
-            printStream.println("Project:")
-            printStream.println()
-            printStream.println(projectPath.readText())
-            printStream.println("Project.Program.cs:")
-            printStream.println()
-            printStream.println(projectPath.parent.resolve("Program.cs").readText())
+            printStream.printAspireFiles(solution, hostProjectPath, sharedProjectPath)
+            for (projectPath in projectPaths) {
+                printStream.printProjectFiles(projectPath)
+            }
         }
+    }
+
+    private fun PrintStream.printAspireFiles(solution: File, hostProjectPath: Path, sharedProjectPath: Path) {
+        println("Solution file:")
+        println()
+        println(solution.readText())
+        println()
+        println("AppHost:")
+        println()
+        println(hostProjectPath.readText())
+        println()
+        println("AppHost.Program.cs:")
+        println()
+        println(hostProjectPath.parent.resolve("Program.cs").readText())
+        println()
+        println("ServiceDefaults:")
+        println()
+        println(sharedProjectPath.readText())
+        println()
+    }
+
+    private fun PrintStream.printProjectFiles(projectPath: Path) {
+        println("Project(${projectPath.name}):")
+        println()
+        println(projectPath.readText())
+        println()
+        println("Project(${projectPath.name}) Program.cs:")
+        println()
+        println(projectPath.parent.resolve("Program.cs").readText())
+        println()
     }
 }
