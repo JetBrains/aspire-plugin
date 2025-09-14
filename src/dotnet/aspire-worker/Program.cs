027717e876fb077a5d05b4f1f81fe1320bd62f9a
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2025-09-14 20:32:37 +0200

    Create a project for integrations tests

--- src/dotnet/aspire-worker/Program.cs
+++ src/dotnet/aspire-worker/Program.cs
@@ -54,3 +54,5 @@ finally
 {
     Log.CloseAndFlush();
 }
+
+public partial class Program;
---
+++ src/dotnet/aspire-worker/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("AspireWorkerIntegrationTests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
