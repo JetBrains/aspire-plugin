b9bc0391f428ea4171e0e2477bc9c5f65542ea10
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2025-09-20 16:24:26 +0200

    Create a first integration test

--- AspireWorkerIntegrationTests/AspireWorkerWebApplicationFactory.cs
+++ AspireWorkerIntegrationTests/AspireWorkerWebApplicationFactory.cs
@@ -1,5 +1,22 @@
+using System.Linq;
+using JetBrains.Rider.Aspire.Worker.AspireHost;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace JetBrains.Rider.Aspire.Worker.IntegrationTests;
 
-public class AspireWorkerWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class;
+public class AspireWorkerWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var aspireHostService = services.SingleOrDefault(d => d.ServiceType == typeof(InMemoryAspireHostService));
+            if (aspireHostService != null) services.Remove(aspireHostService);
+            var inMemoryAspireHostService = new  InMemoryAspireHostService();
+            services.AddSingleton<IAspireHostService>(inMemoryAspireHostService);
+            services.AddSingleton(inMemoryAspireHostService);
+        });
+    }
+};
