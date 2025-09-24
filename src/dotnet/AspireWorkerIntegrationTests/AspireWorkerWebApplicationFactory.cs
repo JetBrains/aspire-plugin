a8086085fc0736f909780818d38e2aa6f66ee30c
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2025-09-24 15:37:21 +0200

    Add tests for CreateSession endpoint

--- src/dotnet/AspireWorkerIntegrationTests/AspireWorkerWebApplicationFactory.cs
+++ src/dotnet/AspireWorkerIntegrationTests/AspireWorkerWebApplicationFactory.cs
@@ -1,4 +1,5 @@
 using JetBrains.Rider.Aspire.Worker.AspireHost;
+using JetBrains.Rider.Aspire.Worker.Configuration;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,15 +8,19 @@ namespace JetBrains.Rider.Aspire.Worker.IntegrationTests;
 
 public class AspireWorkerWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
 {
+    public const string TestToken = "test-token";
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureServices(services =>
         {
             var aspireHostService = services.SingleOrDefault(d => d.ServiceType == typeof(InMemoryAspireHostService));
             if (aspireHostService != null) services.Remove(aspireHostService);
-            var inMemoryAspireHostService = new  InMemoryAspireHostService();
+            var inMemoryAspireHostService = new InMemoryAspireHostService();
             services.AddSingleton<IAspireHostService>(inMemoryAspireHostService);
             services.AddSingleton(inMemoryAspireHostService);
+
+            services.Configure<DcpSessionOptions>(opts => opts.Token = TestToken);
         });
     }
 }
--- src/dotnet/AspireWorkerIntegrationTests/SessionEndpointTests.cs
+++ src/dotnet/AspireWorkerIntegrationTests/SessionEndpointTests.cs
@@ -1,7 +1,10 @@
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
+using AutoFixture;
 using JetBrains.Rider.Aspire.Worker.Sessions;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace JetBrains.Rider.Aspire.Worker.IntegrationTests;
 
@@ -24,5 +27,68 @@ public class SessionEndpointTests(AspireWorkerWebApplicationFactory<Program> fac
         Assert.NotNull(info);
         var version = Assert.Single(info.ProtocolsSupported);
         Assert.Equal("2024-03-03", version);
+    }
+
+    [Fact]
+    public async Task CreateSessionEndpointReturns201AndStoresSession()
+    {
+        var fixture = new Fixture();
+        var session = fixture.Create<Session>();
+        var dcpInstanceId = fixture.Create<string>();
+
+        var client = factory.CreateClient();
+        client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", AspireWorkerWebApplicationFactory<Program>.TestToken);
+        client.DefaultRequestHeaders.Add("Microsoft-Developer-DCP-Instance-ID", dcpInstanceId);
+
+        var response = await client.PutAsJsonAsync("/run_session/?api-version=2024-03-03", session, _jsonOptions);
+
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        var location = response.Headers.Location?.OriginalString;
+        Assert.NotNull(location);
+        const string runSessionPrefix = "/run_session/";
+        Assert.StartsWith(runSessionPrefix, location);
+        var sessionId = location[runSessionPrefix.Length..];
+        var hostService = factory.Services.GetRequiredService<InMemoryAspireHostService>();
+        var storedSession = hostService.GetSession(sessionId);
+        Assert.NotNull(storedSession);
+        Assert.Equal(dcpInstanceId[..5], storedSession.Value.HostId);
+        Assert.Equivalent(session, storedSession.Value.Session);
+    }
+
+    [Fact]
+    public async Task CreateSessionEndpointWithInvalidApiVersionReturns400()
+    {
+        var fixture = new Fixture();
+        var session = fixture.Create<Session>();
+        var dcpInstanceId = fixture.Create<string>();
+
+        var client = factory.CreateClient();
+        client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", AspireWorkerWebApplicationFactory<Program>.TestToken);
+        client.DefaultRequestHeaders.Add("Microsoft-Developer-DCP-Instance-ID", dcpInstanceId);
+
+        var response = await client.PutAsJsonAsync("/run_session/?api-version=wrong", session, _jsonOptions);
+
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        var error = await response.Content.ReadFromJsonAsync<ErrorResponse>(_jsonOptions);
+        Assert.NotNull(error);
+        Assert.Equal("ProtocolVersionIsNotSupported", error.Error.Code);
+    }
+
+    [Fact]
+    public async Task CreateSessionEndpointWithInvalidTokenReturns403()
+    {
+        var fixture = new Fixture();
+        var session = fixture.Create<Session>();
+        var dcpInstanceId = fixture.Create<string>();
+
+        var client = factory.CreateClient();
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "wrong-token");
+        client.DefaultRequestHeaders.Add("Microsoft-Developer-DCP-Instance-ID", dcpInstanceId);
+
+        var response = await client.PutAsJsonAsync("/run_session/?api-version=2024-03-03", session, _jsonOptions);
+
+        Assert.Equal(HttpStatusCode.Forbidden, response.StatusCode);
     }
 }
