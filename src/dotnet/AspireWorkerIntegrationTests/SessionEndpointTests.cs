639fe794f8bdcf3c0dfa81660c3051f24c726458
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2025-09-24 15:51:31 +0200

    Add tests for DeleteSession endpoint

--- src/dotnet/AspireWorkerIntegrationTests/SessionEndpointTests.cs
+++ src/dotnet/AspireWorkerIntegrationTests/SessionEndpointTests.cs
@@ -91,4 +91,79 @@ public class SessionEndpointTests(AspireWorkerWebApplicationFactory<Program> fac
 
         Assert.Equal(HttpStatusCode.Forbidden, response.StatusCode);
     }
+
+    [Fact]
+    public async Task DeleteSessionEndpointReturns200AndDeleteSession()
+    {
+        var fixture = new Fixture();
+        var session = fixture.Create<Session>();
+        var dcpInstanceId = fixture.Create<string>();
+
+        var client = factory.CreateClient();
+        client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", AspireWorkerWebApplicationFactory<Program>.TestToken);
+        client.DefaultRequestHeaders.Add("Microsoft-Developer-DCP-Instance-ID", dcpInstanceId);
+
+        var createResponse = await client.PutAsJsonAsync("/run_session/?api-version=2024-03-03", session, _jsonOptions);
+        var location = createResponse.Headers.Location?.OriginalString;
+        Assert.NotNull(location);
+        var sessionId = location["/run_session/".Length..];
+
+        var deleteResponse = await client.DeleteAsync($"/run_session/{sessionId}?api-version=2024-03-03");
+        Assert.Equal(HttpStatusCode.OK, deleteResponse.StatusCode);
+        var hostService = factory.Services.GetRequiredService<InMemoryAspireHostService>();
+        var storedSession = hostService.GetSession(sessionId);
+        Assert.Null(storedSession);
+    }
+
+    [Fact]
+    public async Task DeleteSessionEndpointWithInvalidApiVersionReturns400()
+    {
+        var fixture = new Fixture();
+        var dcpInstanceId = fixture.Create<string>();
+
+        var client = factory.CreateClient();
+        client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", AspireWorkerWebApplicationFactory<Program>.TestToken);
+        client.DefaultRequestHeaders.Add("Microsoft-Developer-DCP-Instance-ID", dcpInstanceId);
+
+        var response = await client.DeleteAsync("/run_session/some-id?api-version=wrong");
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        var error = await response.Content.ReadFromJsonAsync<ErrorResponse>(_jsonOptions);
+        Assert.NotNull(error);
+        Assert.Equal("ProtocolVersionIsNotSupported", error.Error.Code);
+    }
+
+    [Fact]
+    public async Task DeleteSessionEndpointWithInvalidTokenReturns403()
+    {
+        var fixture = new Fixture();
+        var dcpInstanceId = fixture.Create<string>();
+
+        var client = factory.CreateClient();
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "wrong-token");
+        client.DefaultRequestHeaders.Add("Microsoft-Developer-DCP-Instance-ID", dcpInstanceId);
+
+        var response = await client.DeleteAsync("/run_session/some-id?api-version=2024-03-03");
+        Assert.Equal(HttpStatusCode.Forbidden, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task DeleteSessionEndpointWithUnknownSessionReturns400()
+    {
+        var fixture = new Fixture();
+        var sessionId = fixture.Create<string>();
+        var dcpInstanceId = fixture.Create<string>();
+
+        var client = factory.CreateClient();
+        client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", AspireWorkerWebApplicationFactory<Program>.TestToken);
+        client.DefaultRequestHeaders.Add("Microsoft-Developer-DCP-Instance-ID", dcpInstanceId);
+
+        var response = await client.DeleteAsync($"/run_session/{sessionId}?api-version=2024-03-03");
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        var error = await response.Content.ReadFromJsonAsync<ErrorResponse>(_jsonOptions);
+        Assert.NotNull(error);
+        Assert.Equal("AspireHostIsNotFound", error.Error.Code);
+    }
 }
