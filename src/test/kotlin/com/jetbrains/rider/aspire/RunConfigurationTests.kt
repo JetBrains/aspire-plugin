5bd0939d773ed4be979aece8ca69972d379bbf4d
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2025-09-25 15:29:43 +0200

    Update tests

--- src/test/kotlin/com/jetbrains/rider/aspire/RunConfigurationTests.kt
+++ src/test/kotlin/com/jetbrains/rider/aspire/RunConfigurationTests.kt
@@ -2,16 +2,16 @@ package com.jetbrains.rider.aspire
 
 import com.jetbrains.rider.test.OpenSolutionParams
 import com.jetbrains.rider.test.annotations.Solution
-import com.jetbrains.rider.test.annotations.TestEnvironment
+import com.jetbrains.rider.test.annotations.TestSettings
 import com.jetbrains.rider.test.base.PerClassSolutionTestBase
-import com.jetbrains.rider.test.env.enums.BuildTool
-import com.jetbrains.rider.test.env.enums.SdkVersion
+import com.jetbrains.rider.test.enums.BuildTool
+import com.jetbrains.rider.test.enums.sdk.SdkVersion
 import com.jetbrains.rider.test.facades.solution.RiderSolutionApiFacade
 import com.jetbrains.rider.test.facades.solution.SolutionApiFacade
 import com.jetbrains.rider.test.framework.executeWithGold
 import org.testng.annotations.Test
 
-@TestEnvironment(sdkVersion = SdkVersion.AUTODETECT, buildTool = BuildTool.AUTODETECT)
+@TestSettings(sdkVersion = SdkVersion.AUTODETECT, buildTool = BuildTool.AUTODETECT)
 @Solution("DefaultAspireSolution")
 class RunConfigurationTests : PerClassSolutionTestBase() {
     override val solutionApiFacade: SolutionApiFacade = object : RiderSolutionApiFacade() {
