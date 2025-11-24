4e0f766a5b91b50b76586eaff4754c79ca8a8e0b
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2025-11-24 16:21:56 +0100

    feat: implement `AspireFileExecutorFactory`

--- src/main/kotlin/com/jetbrains/rider/aspire/run/file/AspireFileExecutorFactory.kt
+++ src/main/kotlin/com/jetbrains/rider/aspire/run/file/AspireFileExecutorFactory.kt
@@ -8,6 +8,8 @@ import com.intellij.openapi.project.Project
 import com.jetbrains.rd.util.lifetime.Lifetime
 import com.jetbrains.rider.aspire.run.AspireExecutorFactory
 import com.jetbrains.rider.aspire.run.states.AspireHostRunProfileState
+import com.jetbrains.rider.aspire.util.getStartBrowserAction
+import com.jetbrains.rider.run.configurations.TerminalMode
 import com.jetbrains.rider.runtime.DotNetExecutable
 import com.jetbrains.rider.runtime.RiderDotNetActiveRuntimeHost
 import com.jetbrains.rider.runtime.dotNetCore.DotNetCoreRuntime
@@ -21,6 +23,47 @@ internal class AspireFileExecutorFactory(
         environment: ExecutionEnvironment,
         lifetime: Lifetime
     ): RunProfileState {
-        TODO("Not yet implemented")
+        val activeRuntime = RiderDotNetActiveRuntimeHost.getInstance(project).dotNetCoreRuntime.value
+            ?: throw CantRunException("Unable to find appropriate dotnet runtime")
+
+        val executable = getDotNetExecutable(activeRuntime)
+
+        return when (executorId) {
+            DefaultRunExecutor.EXECUTOR_ID -> AspireHostRunProfileState(executable, activeRuntime, environment)
+            else -> throw CantRunException("Unable to execute Aspire host with $executorId executor")
+        }
+    }
+
+    private suspend fun getDotNetExecutable(
+        activeRuntime: DotNetCoreRuntime
+    ): DotNetExecutable {
+        val effectiveArguments = buildString {
+            append("--file ")
+            append(parameters.filePath)
+        }
+
+        val effectiveEnvs = parameters.envs.toMutableMap()
+        val environmentVariableValues = configureEnvironmentVariables(effectiveEnvs, activeRuntime)
+
+        var effectiveUrl = parameters.startBrowserParameters.url
+        if (environmentVariableValues.browserToken != null) {
+            effectiveUrl = configureUrl(effectiveUrl, environmentVariableValues.browserToken)
+        }
+
+        val effectiveLaunchBrowser = parameters.startBrowserParameters.startAfterLaunch
+
+        return DotNetExecutable(
+            activeRuntime.cliExePath,
+            null,
+            parameters.workingDirectory,
+            effectiveArguments,
+            TerminalMode.Auto,
+            effectiveEnvs,
+            true,
+            getStartBrowserAction(effectiveUrl, effectiveLaunchBrowser, parameters.startBrowserParameters),
+            null,
+            "",
+            true
+        )
     }
 }
