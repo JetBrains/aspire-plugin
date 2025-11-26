d09055bf6f0dcc7786e80812310c2301c88b1b1d
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2025-11-26 10:05:21 +0100

    build: upgrade Gradle to 9.2.1 and update related properties and links

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,7 +48,7 @@ repositories {
     }
 }
 
-// Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
+// Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/version_catalogs.html
 dependencies {
     implementation(libs.serializationJson)
     testImplementation(libs.opentest4j)
@@ -115,7 +115,7 @@ intellijPlatform {
         token = providers.environmentVariable("PUBLISH_TOKEN")
         // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
-        // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
+        // https://plugins.jetbrains.com/docs/intellij/publishing-plugin.html#specifying-a-release-channel
         channels = providers.gradleProperty("pluginVersion")
             .map { listOf(it.substringAfter('-', "").substringBefore('.').ifEmpty { "default" }) }
         hidden = true
