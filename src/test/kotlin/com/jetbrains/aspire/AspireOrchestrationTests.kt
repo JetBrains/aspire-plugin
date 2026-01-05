8dd8c6681861c770ed83d9e7cef3ecc74ed6be0b
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2026-01-05 15:45:42 +0100

    add a test for aspire-maui orchestration

--- src/test/kotlin/com/jetbrains/aspire/AspireOrchestrationTests.kt
+++ src/test/kotlin/com/jetbrains/aspire/AspireOrchestrationTests.kt
@@ -18,7 +18,6 @@ import com.jetbrains.rider.test.framework.persistAllFilesOnDisk
 import com.jetbrains.rider.test.framework.runner.IntegrationTestRunner
 import com.jetbrains.rider.test.scriptingApi.*
 import org.testng.annotations.Test
-import java.io.File
 import java.io.PrintStream
 import java.nio.file.Path
 import kotlin.io.path.exists
@@ -33,36 +32,51 @@ class AspireOrchestrationTests : PerTestSolutionTestBase() {
         params.restoreNuGetPackages = true
         params.waitForCaches = true
         params.waitForSolutionBuilder = true
+        params.restoreNuGetPackages = true
     }
 
-    override val testRunner: IntegrationTestRunner by lazy { IntegrationTestRunner(testProcessor,
-        aspireLoggedErrorProcessor
-    ) }
+    override val testRunner: IntegrationTestRunner by lazy {
+        IntegrationTestRunner(
+            testProcessor,
+            aspireLoggedErrorProcessor
+        )
+    }
 
     @Test
     @Solution("DefaultAspNetCoreSolution")
     fun `Add aspire orchestration for asp net core project`() {
-        runTest(listOf(activeSolution), "$activeSolution.AppHost", "$activeSolution.ServiceDefaults")
+        runTest()
     }
 
     @Test
     @Solution("SolutionWithMultipleAspNetCoreProjects")
     fun `Add aspire orchestration for multiple asp net core projects`() {
         val projectNames = listOf("Project1", "Project2", "Project3")
-        runTest(projectNames, "$activeSolution.AppHost", "$activeSolution.ServiceDefaults")
+        runTest(projectNames = projectNames)
     }
 
     @Test
     @Solution("DefaultWorkerSolution")
     fun `Add aspire orchestration for worker project`() {
-        runTest(listOf(activeSolution), "$activeSolution.AppHost", "$activeSolution.ServiceDefaults")
+        runTest()
     }
 
-    private fun runTest(projectNames: List<String>, appHostName: String, serviceDefaultsName: String) {
+    @Test
+    @Solution("DefaultMauiSolution")
+    fun `Add aspire orchestration for maui project`() {
+        runTest(serviceDefaultsName = "DefaultMauiSolution.Maui.ServiceDefaults", projectMainFileName = "MauiProgram.cs")
+    }
+
+    private fun runTest(
+        projectNames: List<String> = listOf(activeSolution),
+        appHostName: String = "$activeSolution.AppHost",
+        serviceDefaultsName: String = "$activeSolution.ServiceDefaults",
+        projectMainFileName: String = "Program.cs"
+    ) {
         prepareProjectView(project)
 
         val solutionDirectoryPath = activeSolutionDirectory.toPath()
-        val solution = activeSolutionDirectory.resolve("$activeSolution.sln")
+        val solution = activeSolutionDirectory.toPath().resolve("$activeSolution.sln")
         val hostProjectPath =
             solutionDirectoryPath.resolve(appHostName).resolve("$appHostName.csproj")
         val sharedProjectPath =
@@ -90,12 +104,12 @@ class AspireOrchestrationTests : PerTestSolutionTestBase() {
         executeWithGold(testGoldFile) { printStream ->
             printStream.printAspireFiles(solution, hostProjectPath, sharedProjectPath)
             for (projectPath in projectPaths) {
-                printStream.printProjectFiles(projectPath)
+                printStream.printProjectFiles(projectPath, projectMainFileName)
             }
         }
     }
 
-    private fun PrintStream.printAspireFiles(solution: File, hostProjectPath: Path, sharedProjectPath: Path) {
+    private fun PrintStream.printAspireFiles(solution: Path, hostProjectPath: Path, sharedProjectPath: Path) {
         println("Solution file:")
         println()
         println(solution.readText())
@@ -114,14 +128,14 @@ class AspireOrchestrationTests : PerTestSolutionTestBase() {
         println()
     }
 
-    private fun PrintStream.printProjectFiles(projectPath: Path) {
+    private fun PrintStream.printProjectFiles(projectPath: Path, projectMainFileName: String) {
         println("Project(${projectPath.name}):")
         println()
         println(projectPath.readText())
         println()
-        println("Project(${projectPath.name}) Program.cs:")
+        println("Project(${projectPath.name}) $projectMainFileName:")
         println()
-        println(projectPath.parent.resolve("Program.cs").readText())
+        println(projectPath.parent.resolve(projectMainFileName).readText())
         println()
     }
 }
---
+++ testData/solutions/DefaultMauiSolution/DefaultMauiSolution/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DefaultMauiSolution;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
