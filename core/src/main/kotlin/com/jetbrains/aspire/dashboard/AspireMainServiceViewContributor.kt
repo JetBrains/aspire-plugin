9f6b84c30f1729500349e82fd721a18136a1a6c4
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2026-01-28 14:57:11 +0100

    Fix: `Aspire` service node is shown even if there is no Aspire run configs

--- core/src/main/kotlin/com/jetbrains/aspire/dashboard/AspireMainServiceViewContributor.kt
+++ core/src/main/kotlin/com/jetbrains/aspire/dashboard/AspireMainServiceViewContributor.kt
@@ -5,12 +5,15 @@ import com.intellij.execution.services.ServiceViewLazyContributor
 import com.intellij.execution.services.SimpleServiceViewDescriptor
 import com.intellij.openapi.project.Project
 import com.jetbrains.aspire.AspireIcons
+import com.jetbrains.aspire.worker.AspireWorker
 
 class AspireMainServiceViewContributor : ServiceViewContributor<AspireWorkerViewModel>, ServiceViewLazyContributor {
     override fun getViewDescriptor(project: Project) =
         SimpleServiceViewDescriptor("Aspire", AspireIcons.Service)
 
     override fun getServices(project: Project): List<AspireWorkerViewModel> {
+        if (AspireWorker.getInstance(project).appHosts.value.isEmpty()) return emptyList()
+
         val vm = AspireWorkerViewModelFactory.getInstance(project).create()
         return listOf(vm)
     }
