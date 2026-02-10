5b64fe06dab21cc826a3a714c84921031fb7b147
Phantom <P_hanto_m@outlook.com>
2026-02-10 16:53:44 +0400

    Fix Code Review problems

--- rider/src/main/kotlin/com/jetbrains/aspire/rider/launchProfiles/LaunchSettingsJsonServiceExtensions.kt
+++ rider/src/main/kotlin/com/jetbrains/aspire/rider/launchProfiles/LaunchSettingsJsonServiceExtensions.kt
@@ -5,14 +5,14 @@ import com.jetbrains.rider.run.configurations.controls.LaunchProfile
 import com.jetbrains.rider.run.configurations.launchSettings.LaunchSettingsJson
 import com.jetbrains.rider.run.configurations.launchSettings.LaunchSettingsJsonService
 import java.nio.file.Path
-import kotlin.collections.orEmpty
 import kotlin.io.path.nameWithoutExtension
 
 fun LaunchSettingsJsonService.getFirstOrNullLaunchProfileProfile(runnableProject: RunnableProject): LaunchProfile? {
     val profiles = loadLaunchSettings(runnableProject)?.profiles ?: return null
 
     return profiles
-        .asSequence().firstOrNull { it.value.commandName.equals("Project", true) }
+        .asSequence()
+        .firstOrNull { it.value.commandName.equals("Project", true) }
         ?.let { LaunchProfile(it.key, it.value) }
 }
 
--- rider/src/main/kotlin/com/jetbrains/aspire/rider/run/file/BuildAspireFileBeforeRunTask.kt
+++ rider/src/main/kotlin/com/jetbrains/aspire/rider/run/file/BuildAspireFileBeforeRunTask.kt
@@ -2,4 +2,4 @@ package com.jetbrains.aspire.rider.run.file
 
 import com.jetbrains.rider.build.tasks.BeforeRunTaskWithProject
 
-class BuildAspireFileBeforeRunTask : BeforeRunTaskWithProject<BuildAspireFileBeforeRunTask>(BuildAspireFileBeforeRunTaskProvider.providerId)
+internal class BuildAspireFileBeforeRunTask : BeforeRunTaskWithProject<BuildAspireFileBeforeRunTask>(providerId)
