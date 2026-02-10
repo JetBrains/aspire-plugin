f3a50d31e7c03aa0512c93adc3d9f827ffbbb240
Phantom <P_hanto_m@outlook.com>
2026-02-10 16:53:44 +0400

    Fix Code Review problems

--- rider/src/main/kotlin/com/jetbrains/aspire/rider/run/file/BuildAspireFileBeforeRunTaskProvider.kt
+++ rider/src/main/kotlin/com/jetbrains/aspire/rider/run/file/BuildAspireFileBeforeRunTaskProvider.kt
@@ -3,15 +3,14 @@
 package com.jetbrains.aspire.rider.run.file
 
 import com.intellij.execution.configurations.RunConfiguration
-import com.intellij.execution.executors.DefaultDebugExecutor
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.diagnostic.runAndLogException
 import com.intellij.openapi.progress.runBlockingMaybeCancellable
 import com.intellij.openapi.util.Key
 import com.intellij.platform.ide.progress.withBackgroundProgress
-import com.jetbrains.rd.platform.util.getLogger
 import com.jetbrains.rd.util.lifetime.Lifetime
 import com.jetbrains.rd.util.threading.coroutines.asCoroutineDispatcher
 import com.jetbrains.rd.util.threading.coroutines.nextTrueValue
@@ -29,92 +28,94 @@ import org.jetbrains.annotations.Nls
 import javax.swing.Icon
 import kotlin.io.path.pathString
 
-class BuildAspireFileBeforeRunTaskProvider: BeforeRunTaskWithProjectProvider<BuildAspireFileBeforeRunTask>()  {
-  companion object {
-    val providerId = Key.create<BuildAspireFileBeforeRunTask>("Build")
+internal class BuildAspireFileBeforeRunTaskProvider : BeforeRunTaskWithProjectProvider<BuildAspireFileBeforeRunTask>() {
+    companion object {
 
-    @Nls
-    private val TASK_NAME = RiderBuildBundle.message("BuildProjectBeforeRunTaskProvider.task.name.build.project")
-    val logger = getLogger<BuildAspireFileBeforeRunTaskProvider>()
-  }
-
-  override val baseDescription: @Nls String
-    get() = RiderBuildBundle.message("description.build.project")
-
-  override fun getId(): Key<BuildAspireFileBeforeRunTask> = providerId
-
-  override fun getName(): String = TASK_NAME
-
-  override fun getIcon(): Icon = AllIcons.Actions.Compile
-
-  override fun shouldCreateBuildBeforeRunTaskByDefault(runConfiguration: RunConfiguration): Boolean {
-    return runConfiguration is AspireFileConfiguration
-  }
-
-  override fun createTask(configuration: RunConfiguration): BuildAspireFileBeforeRunTask? {
-    if(!shouldCreateBuildBeforeRunTaskByDefault(configuration)) return null
-
-    return BuildAspireFileBeforeRunTask().apply {
-      isEnabled = true
-    }
-  }
-
-  override fun canExecuteTask(configuration: RunConfiguration, task: BuildAspireFileBeforeRunTask): Boolean {
-    return BuildHost.getInstance(configuration.project).ready.value
-  }
-
-  override fun executeTask(
-    context: DataContext,
-    configuration: RunConfiguration,
-    env: ExecutionEnvironment,
-    task: BuildAspireFileBeforeRunTask
-  ): Boolean {
-    if (configuration !is AspireFileConfiguration) {
-      logger.warn("configuration is not AspireFileConfiguration")
-      return false
+        val LOG = logger<BuildAspireFileBeforeRunTaskProvider>()
     }
 
-    val buildHost = BuildHost.getInstance(configuration.project)
-    if (!buildHost.ready.value) {
-      logger.warn("buildHost is not ready")
-      return false
+    override val baseDescription: @Nls String
+        get() = RiderBuildBundle.message("description.build.project")
+
+    override fun getId(): Key<BuildAspireFileBeforeRunTask> = providerId
+
+    override fun getName(): String = TASK_NAME
+
+    override fun getIcon(): Icon = AllIcons.Actions.Compile
+
+    override fun shouldCreateBuildBeforeRunTaskByDefault(runConfiguration: RunConfiguration): Boolean {
+        return runConfiguration is AspireFileConfiguration
     }
 
-    return Lifetime.using { buildSessionLt ->
-      logger.runAndLogException {
-        return@runAndLogException runBlockingMaybeCancellable {
-          return@runBlockingMaybeCancellable buildWithProgress(env, configuration, buildSessionLt)
+    override fun createTask(configuration: RunConfiguration): BuildAspireFileBeforeRunTask? {
+        if (!shouldCreateBuildBeforeRunTaskByDefault(configuration)) return null
+
+        return BuildAspireFileBeforeRunTask().apply {
+            isEnabled = true
         }
-      } == true
-    }
-  }
-
-  private suspend fun buildWithProgress(
-    env: ExecutionEnvironment,
-    configuration: AspireFileConfiguration,
-    buildSessionLt: Lifetime,
-  ): Boolean = withBackgroundProgress(env.project, RiderBundle.message("progress.title.generating.project.file")) {
-    val projectToBuildPath = configuration.tryGetProjectFilePath(buildSessionLt)
-
-    if (projectToBuildPath == null) {
-      logger.warn("projectToBuildPath is null")
-      return@withBackgroundProgress false
     }
 
-    val selectedProjectsForBuild = listOf(projectToBuildPath.pathString)
-    val buildTaskThrottler = BuildTaskThrottler.getInstance(configuration.project)
-
-    val project = env.project
-    val session = project.frontendProjectSession.appSession
-    val protocol = session.protocol
-
-    withContext(protocol.scheduler.asCoroutineDispatcher) {
-      BuildHost.getInstance(project).buildModel.ready.nextTrueValue()
+    override fun canExecuteTask(configuration: RunConfiguration, task: BuildAspireFileBeforeRunTask): Boolean {
+        return BuildHost.getInstance(configuration.project).ready.value
     }
 
-    val buildStatus = buildTaskThrottler.buildSequentially(
-      BuildParameters(BuildTarget(), selectedProjectsForBuild, isSingleProjectBuild = true)
-    )
-    return@withBackgroundProgress buildStatus.msBuildStatus
-  }
+    override fun executeTask(
+        context: DataContext,
+        configuration: RunConfiguration,
+        env: ExecutionEnvironment,
+        task: BuildAspireFileBeforeRunTask
+    ): Boolean {
+        if (configuration !is AspireFileConfiguration) {
+            LOG.warn("configuration is not AspireFileConfiguration")
+            return false
+        }
+
+        val buildHost = BuildHost.getInstance(configuration.project)
+        if (!buildHost.ready.value) {
+            LOG.warn("buildHost is not ready")
+            return false
+        }
+
+        return Lifetime.using { buildSessionLt ->
+            LOG.runAndLogException {
+                return@runAndLogException runBlockingMaybeCancellable {
+                    return@runBlockingMaybeCancellable buildWithProgress(env, configuration, buildSessionLt)
+                }
+            } == true
+        }
+    }
+
+    private suspend fun buildWithProgress(
+        env: ExecutionEnvironment,
+        configuration: AspireFileConfiguration,
+        buildSessionLt: Lifetime,
+    ): Boolean = withBackgroundProgress(env.project, RiderBundle.message("progress.title.generating.project.file")) {
+        val projectToBuildPath = configuration.tryGetProjectFilePath(buildSessionLt)
+
+        if (projectToBuildPath == null) {
+            LOG.warn("projectToBuildPath is null")
+            return@withBackgroundProgress false
+        }
+
+        val selectedProjectsForBuild = listOf(projectToBuildPath.pathString)
+        val buildTaskThrottler = BuildTaskThrottler.getInstance(configuration.project)
+
+        val project = env.project
+        val session = project.frontendProjectSession.appSession
+        val protocol = session.protocol
+
+        withContext(protocol.scheduler.asCoroutineDispatcher) {
+            BuildHost.getInstance(project).buildModel.ready.nextTrueValue()
+        }
+
+        val buildStatus = buildTaskThrottler.buildSequentially(
+            BuildParameters(BuildTarget(), selectedProjectsForBuild, isSingleProjectBuild = true)
+        )
+        return@withBackgroundProgress buildStatus.msBuildStatus
+    }
 }
+
+internal val providerId = Key.create<BuildAspireFileBeforeRunTask>("BuildAspireFile")
+
+@Nls
+private val TASK_NAME = RiderBuildBundle.message("BuildProjectBeforeRunTaskProvider.task.name.build.project")
