16812e54d4a19b980c4acbef9ec03e7965db2eec
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2026-02-13 18:30:34 +0100

    Add python module

---
+++ python/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    alias(libs.plugins.kotlin)
+    id("org.jetbrains.intellij.platform.module")
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+repositories {
+    mavenCentral()
+
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        intellijIdea(providers.gradleProperty("ideaVersion")) {
+            useCache = true
+        }
+
+        compatiblePlugin("PythonCore")
+    }
+
+    implementation(project(":core"))
+}
