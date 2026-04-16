d07cacce2cd0117c493d71873f30c68d7b8fe53b
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2026-04-16 11:43:45 +0200

    Show frontend icons for resource gutter marks

---
+++ src/dotnet/AspirePlugin/AspireIconIds.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.RdBackend.Common.Features.Icons;
+using JetBrains.UI.Icons;
+
+namespace JetBrains.Rider.Aspire.Plugin;
+
+internal static class AspireIconIds
+{
+    internal static readonly IconId StartResourceIconId = new FrontendIconId("icons/startResource.svg");
+    internal static readonly IconId StopResourceIconId = new FrontendIconId("icons/stopResource.svg");
+    internal static readonly IconId RestartResourceIconId = new FrontendIconId("icons/restartResource.svg");
+    internal static readonly IconId RebuildResourceIconId = new FrontendIconId("icons/rebuildResource.svg");
+}
