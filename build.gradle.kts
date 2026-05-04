3e6908fa2b4c47e34da3e1dd9a90dc61a8ad7808
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2026-05-04 14:14:28 +0200

    Update plugin according to the latest template

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,44 +64,9 @@ dependencies {
 
 // Configure IntelliJ Platform Gradle Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-extension.html
 intellijPlatform {
-    pluginConfiguration {
-        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-        description = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
-            val start = "<!-- Plugin description -->"
-            val end = "<!-- Plugin description end -->"
-
-            with(it.lines()) {
-                if (!containsAll(listOf(start, end))) {
-                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
-                }
-                subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
-            }
-        }
-
-        val changelog = project.changelog // local variable for configuration cache compatibility
-        // Get the latest available change notes from the changelog file
-        changeNotes = version.map { pluginVersion ->
-            with(changelog) {
-                renderItem(
-                    (getOrNull(pluginVersion) ?: getUnreleased())
-                        .withHeader(false)
-                        .withEmptySections(false),
-                    Changelog.OutputType.HTML,
-                )
-            }
-        }
-    }
-
     publishing {
         hidden = true
     }
-}
-
-// Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
-changelog {
-    groups.empty()
-    repositoryUrl = providers.gradleProperty("pluginRepositoryUrl")
-    versionPrefix = ""
 }
 
 tasks {
@@ -192,10 +157,6 @@ tasks {
         from("$projectDir/src/dotnet/AspireWorker/bin/$dotnetBuildConfiguration/publish") {
             into("${rootProject.name}/AspireWorker")
         }
-    }
-
-    publishPlugin {
-        dependsOn(patchChangelog)
     }
 
     test {
