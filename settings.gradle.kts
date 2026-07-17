d4c4d81767e50152222f176a6362662bad6b6d84
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2026-07-17 08:11:13 +0200

    Update org.jetbrains.intellij.platform

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,7 +29,7 @@ dependencyResolutionManagement {
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-    id("org.jetbrains.intellij.platform.settings") version "2.16.0"
+    id("org.jetbrains.intellij.platform.settings") version "2.18.1"
 }
 
 rootProject.name = "intellij.aspire"
