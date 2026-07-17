81d56f9a923d46af23324480e979f8cf8ab302a4
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2026-07-17 14:43:04 +0200

    Remove WriteLockModalProgress from test errors

--- src/test/kotlin/com/jetbrains/aspire/AspireLoggedErrorProcessor.kt
+++ src/test/kotlin/com/jetbrains/aspire/AspireLoggedErrorProcessor.kt
@@ -6,7 +6,6 @@ import com.jetbrains.rider.test.logging.knownErrors.RiderKnownLogErrors
 
 val aspireLoggedErrorProcessor: RiderLoggedErrorProcessor = RiderLoggedErrorProcessor(
     RiderKnownLogErrors + KnownLogErrors(
-        "WriteLockModalProgress" to { it.contains("This thread holds write lock while trying to invoke a modal progress") },
         "AwtEventsInsideWriteAction" to { it.contains("AWT events are not allowed inside write action") &&  it.contains("com.intellij.openapi.application.impl.NonBlockingFlushQueue")},
         "WorkspaceModelEDT" to { it.contains("com.jetbrains.rider.projectView.workspace.impl.WorkspaceModelUpdater") },
         "SynchronousRefreshUnderReadLock" to { it.contains("Do not perform a synchronous refresh under read lock") },
