2193f243fdf36cacef630febac205722a16021ac
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2026-07-17 11:53:08 +0200

    Remove DebuggerMatchingHandlerNotFound from test errors

--- src/test/kotlin/com/jetbrains/aspire/AspireLoggedErrorProcessor.kt
+++ src/test/kotlin/com/jetbrains/aspire/AspireLoggedErrorProcessor.kt
@@ -6,7 +6,6 @@ import com.jetbrains.rider.test.logging.knownErrors.RiderKnownLogErrors
 
 val aspireLoggedErrorProcessor: RiderLoggedErrorProcessor = RiderLoggedErrorProcessor(
     RiderKnownLogErrors + KnownLogErrors(
-        "DebuggerMatchingHandlerNotFound" to { it.contains("Haven't found matching handler for") },
         "HighlighterHasNotBeenRegistered" to { it.contains("Removing highlighter hasn't been registered by markup adapter") },
         "RiderHotReloadExecutionListenerDisposable" to { it.contains("com.jetbrains.rider.debugger.dialogs.RiderHotReloadExecutionListener.processStarted") },
         "DoubleReleaseOfEditor" to { it.contains($$"TraceableDisposable$DisposalException: Double release of editor") },
