5a7ee96c607483f56419191477e8f58bf054f258
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2026-07-17 11:51:40 +0200

    Fix debugger tests

--- src/test/kotlin/com/jetbrains/aspire/AspireLoggedErrorProcessor.kt
+++ src/test/kotlin/com/jetbrains/aspire/AspireLoggedErrorProcessor.kt
@@ -13,5 +13,7 @@ val aspireLoggedErrorProcessor: RiderLoggedErrorProcessor = RiderLoggedErrorProc
         "WriteLockModalProgress" to { it.contains("This thread holds write lock while trying to invoke a modal progress") },
         "AwtEventsInsideWriteAction" to { it.contains("AWT events are not allowed inside write action") &&  it.contains("com.intellij.openapi.application.impl.NonBlockingFlushQueue")},
         "WorkspaceModelEDT" to { it.contains("com.jetbrains.rider.projectView.workspace.impl.WorkspaceModelUpdater") },
+        "SynchronousRefreshUnderReadLock" to { it.contains("Do not perform a synchronous refresh under read lock") },
+        "DefaultStacktraceFilter" to { it.contains("Error while applying com.jetbrains.cidr.execution.DefaultStacktraceFilter") },
     )
 )
