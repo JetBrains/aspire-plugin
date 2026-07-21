88270a8da4924a03bdf57e8cbdb08fe400976570
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2026-07-21 08:35:15 +0200

    Remove WorkspaceModelEDT from test errors

--- src/test/kotlin/com/jetbrains/aspire/AspireLoggedErrorProcessor.kt
+++ src/test/kotlin/com/jetbrains/aspire/AspireLoggedErrorProcessor.kt
@@ -6,7 +6,6 @@ import com.jetbrains.rider.test.logging.knownErrors.RiderKnownLogErrors
 
 val aspireLoggedErrorProcessor: RiderLoggedErrorProcessor = RiderLoggedErrorProcessor(
     RiderKnownLogErrors + KnownLogErrors(
-        "WorkspaceModelEDT" to { it.contains("com.jetbrains.rider.projectView.workspace.impl.WorkspaceModelUpdater") },
         "SynchronousRefreshUnderReadLock" to { it.contains("Do not perform a synchronous refresh under read lock") },
         "DefaultStacktraceFilter" to { it.contains("Error while applying com.jetbrains.cidr.execution.DefaultStacktraceFilter") },
     )
