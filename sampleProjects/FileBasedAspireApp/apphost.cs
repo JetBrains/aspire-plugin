ff66aff04859c4a47e8ecd95be663143ad121295
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2026-07-31 14:45:15 +0200

    Add sample file-based Aspire app

---
+++ sampleProjects/FileBasedAspireApp/apphost.cs
@@ -0,0 +1,17 @@
+#:sdk Aspire.AppHost.Sdk@13.3.0
+#:project .\FileBasedAspireApp.ApiService\FileBasedAspireApp.ApiService.csproj
+#:project .\FileBasedAspireApp.Web\FileBasedAspireApp.Web.csproj
+#:property UserSecretsId=0169f63d-5f25-4676-8ffe-ebee3ecb224b
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<Projects.FileBasedAspireApp_ApiService>("apiservice")
+    .WithHttpHealthCheck("/health");
+
+builder.AddProject<Projects.FileBasedAspireApp_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithHttpHealthCheck("/health")
+    .WithReference(apiService)
+    .WaitFor(apiService);
+
+builder.Build().Run();
