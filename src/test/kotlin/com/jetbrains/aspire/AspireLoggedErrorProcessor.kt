11e42bd324934501a15e59e72d2b8f2765b36ac4
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2026-09-03 15:55:31 +0200

    Suppress several errors

--- src/test/kotlin/com/jetbrains/aspire/AspireLoggedErrorProcessor.kt
+++ src/test/kotlin/com/jetbrains/aspire/AspireLoggedErrorProcessor.kt
@@ -10,5 +10,7 @@ val aspireLoggedErrorProcessor: RiderLoggedErrorProcessor = RiderLoggedErrorProc
         "SynchronousRefreshUnderReadLock" to { it.contains("Do not perform a synchronous refresh under read lock") },
         "DefaultStacktraceFilter" to { it.contains("Error while applying com.jetbrains.cidr.execution.DefaultStacktraceFilter") },
         "DebuggerRaisedTargetHitBreakpointEvent" to { it.contains("Exception in debugger process: Debugger raised TargetHitBreakpoint event, but no corresponding breakpoint model found for breakpoint ClrTypeName: System.Diagnostics.Debugger") },
+        "WriteLockModalProgress" to { it.contains("This thread holds write lock while trying to invoke a modal progress.Write actions should be fast so they do not stall the progress in the IDE") },
+        "AWTInsideWriteActions" to { it.contains("AWT events are not allowed inside write action") },
     )
 )
