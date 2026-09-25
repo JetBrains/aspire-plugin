b375d94c0c4c8fd1a7edb3f2493b4e0c50a9c026
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2026-09-25 10:39:43 +0200

    Simplify AspireCliRunConfiguration

--- core/src/main/kotlin/com/jetbrains/aspire/run/cli/AspireCliRunConfiguration.kt
+++ core/src/main/kotlin/com/jetbrains/aspire/run/cli/AspireCliRunConfiguration.kt
@@ -1,6 +1,5 @@
 package com.jetbrains.aspire.run.cli
 
-import com.intellij.execution.CommonProgramRunConfigurationParameters
 import com.intellij.execution.Executor
 import com.intellij.execution.configurations.ConfigurationFactory
 import com.intellij.execution.configurations.RunConfigurationBase
@@ -13,72 +12,11 @@ import com.jetbrains.aspire.AspireCoreBundle
 import kotlin.io.path.Path
 import kotlin.io.path.exists
 
-internal class AspireCliRunConfiguration(
-    project: Project,
-    factory: ConfigurationFactory,
-    name: String
-) : RunConfigurationBase<AspireCliRunConfigurationOptions>(project, factory, name),
-    CommonProgramRunConfigurationParameters {
+internal class AspireCliRunConfiguration(project: Project, factory: ConfigurationFactory, name: String) :
+    RunConfigurationBase<AspireCliRunConfigurationOptions>(project, factory, name) {
 
     internal val cliOptions: AspireCliRunConfigurationOptions
         get() = options as AspireCliRunConfigurationOptions
-
-    override fun getOptionsClass(): Class<AspireCliRunConfigurationOptions> =
-        AspireCliRunConfigurationOptions::class.java
-
-    var appHostFilePath: String?
-        get() = cliOptions.appHostFilePath
-        set(value) {
-            cliOptions.appHostFilePath = value
-        }
-
-    var aspireCliPath: String?
-        get() = cliOptions.aspireCliPath
-        set(value) {
-            cliOptions.aspireCliPath = value
-        }
-
-    var browserUrl: String?
-        get() = cliOptions.browserUrl
-        set(value) {
-            cliOptions.browserUrl = value
-        }
-
-    var startBrowserAfterLaunch: Boolean
-        get() = cliOptions.startBrowserAfterLaunch
-        set(value) {
-            cliOptions.startBrowserAfterLaunch = value
-        }
-
-    var enableIdeDebugging: Boolean
-        get() = cliOptions.enableIdeDebugging
-        set(value) {
-            cliOptions.enableIdeDebugging = value
-        }
-
-    var noBuild: Boolean
-        get() = cliOptions.noBuild
-        set(value) {
-            cliOptions.noBuild = value
-        }
-
-    var isolated: Boolean
-        get() = cliOptions.isolated
-        set(value) {
-            cliOptions.isolated = value
-        }
-
-    var logLevel: AspireCliLogLevel?
-        get() = cliOptions.logLevel
-        set(value) {
-            cliOptions.logLevel = value
-        }
-
-    var usePodmanRuntime: Boolean
-        get() = cliOptions.usePodmanRuntime
-        set(value) {
-            cliOptions.usePodmanRuntime = value
-        }
 
     override fun getConfigurationEditor(): SettingsEditor<AspireCliRunConfiguration> =
         AspireCliSettingsEditor(this)
@@ -87,38 +25,17 @@ internal class AspireCliRunConfiguration(
         AspireCliRunProfileState(this, environment)
 
     override fun checkConfiguration() {
-        val path = appHostFilePath
+        val path = cliOptions.appHostFilePath
         if (path.isNullOrBlank()) {
             throw RuntimeConfigurationError(AspireCoreBundle.message("run.configuration.cli.error.no.app.host"))
         }
         if (!Path(path).exists()) {
-            throw RuntimeConfigurationError(AspireCoreBundle.message("run.configuration.cli.error.app.host.not.found", path))
+            throw RuntimeConfigurationError(
+                AspireCoreBundle.message(
+                    "run.configuration.cli.error.app.host.not.found",
+                    path
+                )
+            )
         }
-    }
-
-
-    override fun getProgramParameters(): String? = cliOptions.arguments
-
-    override fun setProgramParameters(value: String?) {
-        cliOptions.arguments = value
-    }
-
-    override fun getWorkingDirectory(): String? = cliOptions.workingDirectory
-
-    override fun setWorkingDirectory(value: String?) {
-        cliOptions.workingDirectory = value
-    }
-
-    override fun getEnvs(): MutableMap<String, String> = LinkedHashMap(cliOptions.envs)
-
-    override fun setEnvs(envs: MutableMap<String, String>) {
-        cliOptions.envs.clear()
-        cliOptions.envs.putAll(envs)
-    }
-
-    override fun isPassParentEnvs(): Boolean = cliOptions.passParentEnvs
-
-    override fun setPassParentEnvs(passParentEnvs: Boolean) {
-        cliOptions.passParentEnvs = passParentEnvs
     }
 }
