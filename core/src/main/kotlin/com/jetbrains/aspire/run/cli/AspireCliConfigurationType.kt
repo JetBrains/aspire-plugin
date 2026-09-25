ab08fe2478fec745db772c184caf53aa250f1d1e
Rival Abdrakhmanov <rival.abdrakhmanov@jetbrains.com>
2026-09-25 10:08:02 +0200

    Use SimpleConfigurationType instead of a separate ConfigurationFactory

--- core/src/main/kotlin/com/jetbrains/aspire/run/cli/AspireCliConfigurationType.kt
+++ core/src/main/kotlin/com/jetbrains/aspire/run/cli/AspireCliConfigurationType.kt
@@ -1,22 +1,35 @@
 package com.jetbrains.aspire.run.cli
 
-import com.intellij.execution.configurations.ConfigurationTypeBase
+import com.intellij.execution.RunnerAndConfigurationSettings
+import com.intellij.execution.configurations.RunConfiguration
+import com.intellij.execution.configurations.SimpleConfigurationType
+import com.intellij.openapi.components.BaseState
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.NotNullLazyValue
 import com.jetbrains.aspire.AspireCoreBundle
 import com.jetbrains.aspire.AspireIcons
 
-internal class AspireCliConfigurationType : ConfigurationTypeBase(
+internal class AspireCliConfigurationType : SimpleConfigurationType(
     ID,
     AspireCoreBundle.message("run.configuration.cli.name"),
     AspireCoreBundle.message("run.configuration.cli.description"),
-    AspireIcons.RunConfig
-) {
+    NotNullLazyValue.createValue { AspireIcons.RunConfig }
+), DumbAware {
     companion object {
         const val ID = "AspireCliConfiguration"
     }
 
-    val factory = AspireCliConfigurationFactory(this)
+    override fun createTemplateConfiguration(project: Project): RunConfiguration =
+        AspireCliRunConfiguration(project, this, configurationTypeDescription)
 
-    init {
-        addFactory(factory)
+    override fun isEditableInDumbMode(): Boolean = true
+
+    override fun getOptionsClass(): Class<out BaseState> = AspireCliRunConfigurationOptions::class.java
+
+    override fun configureDefaultSettings(settings: RunnerAndConfigurationSettings) {
+        super.configureDefaultSettings(settings)
+        settings.isActivateToolWindowBeforeRun = false
+        settings.isFocusToolWindowBeforeRun = false
     }
 }
